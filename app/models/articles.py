b51509d00ded75edc5f1b4e856b017d486bcdb0b
Kibet 1998 <ronoronald1998@gmail.com>
2021-06-06 11:27:36 +0300

    add view function that returns index page and its data

--- app/models/articles.py
+++ app/models/articles.py
@@ -2,4 +2,10 @@ class Articles:
     '''
     Class that define news objects
     '''
-    def __init__(self,author,title,description,url,image,date)
+    def __init__(self,author,title,description,url,image,date):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
