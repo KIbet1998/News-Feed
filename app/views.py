98cd9c90578817d761300dded00b5eaf36b8de62
Kibet 1998 <ronoronald1998@gmail.com>
2021-06-06 11:44:34 +0300

    add news id function to return news details

--- app/views.py
+++ app/views.py
@@ -7,5 +7,10 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-    message = 'Welcome to News Feed,stay updated'
+    message = 'Welcome to News Feed,Stay updated'
     return render_template('index.html',message=message)
+
+def news(news_id):
+    '''
+    returns news details page and its data
+    '''
